fe913b974cbb8e5d4a27587f6e52bda2b386e318
jesuskaz <jesuskazkid@gmail.com>
2022-01-06 09:58:41 +0200

    Modification base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.43.234/vlisco/';
-// $config['base_url'] = 'http://www.rabisarl.com';
+// $config['base_url'] = 'http://192.168.43.234/vlisco/';
+$config['base_url'] = 'https://www.rabisarl.com';
 
 /*
 |--------------------------------------------------------------------------
